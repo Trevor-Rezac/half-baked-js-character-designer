93d3bdf1fd82b537bcd9c0399bf7af361ed993cf
TrevorRezac <trev.rezac@gmail.com>
2021-12-01 14:58:56 -0800

    working character design with some TDD

--- test/utils.test.js
+++ test/utils.test.js
@@ -6,11 +6,11 @@ const test = QUnit.test;
 test('should return a string describing the current state', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `You clicked on the head 4 times, on the middle 5 times, and on the bottom 1 time. And everybody loves your character's catchphrases:`;
+    const expected = `You have changed the head 2 times, the middle 2 times, and the bottom 3 times!`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = makeStatsString(4, 5, 1);
+    const actual = makeStatsString(2, 2, 3);
 
     //Expect
     // Make assertions about what is expected versus the actual result
